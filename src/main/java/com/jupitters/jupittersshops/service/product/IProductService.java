60cf911782541cbfc79adc92fe92e599a6197f78
jupitters <sampaiobale@gmail.com>
2025-09-23 16:11:30 -0300

    services

---
+++ src/main/java/com/jupitters/jupittersshops/service/product/IProductService.java
@@ -0,0 +1,4 @@
+package com.jupitters.jupittersshops.service.product;
+
+public interface IProductService {
+}
---
+++ src/main/java/com/jupitters/jupittersshops/service/product/ProductService.java
@@ -0,0 +1,4 @@
+package com.jupitters.jupittersshops.service.product;
+
+public class ProductService {
+}
