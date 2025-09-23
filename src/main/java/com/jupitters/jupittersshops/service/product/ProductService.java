cacf016c4fdf24a98060d2dbaaf33e2603fa38fc
jupitters <sampaiobale@gmail.com>
2025-09-23 16:50:03 -0300

    product service

--- src/main/java/com/jupitters/jupittersshops/service/product/ProductService.java
+++ src/main/java/com/jupitters/jupittersshops/service/product/ProductService.java
@@ -1,4 +1,63 @@
 package com.jupitters.jupittersshops.service.product;
 
-public class ProductService {
+import com.jupitters.jupittersshops.model.Product;
+
+import java.util.List;
+
+public class ProductService implements IProductService{
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+
+    }
+
+    @Override
+    public void updateProduct(Product product, Long productId) {
+
+    }
+
+    @Override
+    public List<Product> getProductByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductByBrand(String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductByCategoryAndBrand(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0L;
+    }
 }
