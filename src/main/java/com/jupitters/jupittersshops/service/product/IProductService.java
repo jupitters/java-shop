7474b35a3f333d13ae362681291157b12c2def2c
jupitters <sampaiobale@gmail.com>
2025-09-23 16:22:07 -0300

    interface product service

--- src/main/java/com/jupitters/jupittersshops/service/product/IProductService.java
+++ src/main/java/com/jupitters/jupittersshops/service/product/IProductService.java
@@ -1,4 +1,21 @@
 package com.jupitters.jupittersshops.service.product;
 
+import com.jupitters.jupittersshops.model.Product;
+
+import java.util.List;
+
 public interface IProductService {
+    Product addProduct(Product product);
+    List<Product> getAllProducts();
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    void updateProduct(Product product, Long productId);
+    List<Product> getProductByCategory(String category);
+    List<Product> getProductByBrand(String brand);
+    List<Product> getProductByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrandAndName(String brand, String name);
+    Long countProductsByBrandAndName(String brand, String name);
+
+
 }
