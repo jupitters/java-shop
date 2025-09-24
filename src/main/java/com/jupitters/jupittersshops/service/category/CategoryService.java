2d5e0fc9ba1c78267400a317351499da9173d7b5
jupitters <sampaiobale@gmail.com>
2025-09-24 00:48:08 -0300

    category service

---
+++ src/main/java/com/jupitters/jupittersshops/service/category/CategoryService.java
@@ -0,0 +1,4 @@
+package com.jupitters.jupittersshops.service.category;
+
+public class CategoryService {
+}
---
+++ src/main/java/com/jupitters/jupittersshops/service/category/ICategoryService.java
@@ -0,0 +1,4 @@
+package com.jupitters.jupittersshops.service.category;
+
+public interface ICategoryService {
+}
