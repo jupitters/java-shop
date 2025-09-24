4f77686eaa6ac2ebe0cee4abb0c35c32049d75d1
jupitters <sampaiobale@gmail.com>
2025-09-24 17:19:16 -0300

    category controller: add category

--- src/main/java/com/jupitters/jupittersshops/controller/CategoryController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/CategoryController.java
@@ -1,18 +1,17 @@
 package com.jupitters.jupittersshops.controller;
 
+import com.jupitters.jupittersshops.exceptions.AlreadyExistsException;
 import com.jupitters.jupittersshops.model.Category;
 import com.jupitters.jupittersshops.response.ApiResponse;
 import com.jupitters.jupittersshops.service.category.ICategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RequiredArgsConstructor
@@ -32,13 +31,14 @@ public class CategoryController {
         }
     }
 
+    @PostMapping("/add")
     public ResponseEntity<ApiResponse> addCategory(@RequestBody Category category) {
         try {
             Category newCategory = categoryService.addCategory(category);
             return ResponseEntity.ok(new ApiResponse("Added!", newCategory));
-        } catch (Exception e) {
-            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
-                    .body(new ApiResponse("Error!",  INTERNAL_SERVER_ERROR));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(CONFLICT)
+                    .body(new ApiResponse(e.getMessage(), null));
         }
     }
 
