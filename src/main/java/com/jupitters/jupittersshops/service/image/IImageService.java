1726f8a3bd4447b1c0c839526fb67e7333839c9b
jupitters <sampaiobale@gmail.com>
2025-09-24 14:51:47 -0300

    image service

--- src/main/java/com/jupitters/jupittersshops/service/image/IImageService.java
+++ src/main/java/com/jupitters/jupittersshops/service/image/IImageService.java
@@ -7,5 +7,5 @@ public interface IImageService {
     Image getImageById(Long id);
     void deleteImageById(Long id);
     Image saveImage(MultipartFile file, Long productId);
-    void updateImage(MultipartFile file, Long productId);
+    void updateImage(MultipartFile file, Long imageId);
 }
--- src/main/java/com/jupitters/jupittersshops/service/image/ImageService.java
+++ src/main/java/com/jupitters/jupittersshops/service/image/ImageService.java
@@ -3,14 +3,20 @@ package com.jupitters.jupittersshops.service.image;
 import com.jupitters.jupittersshops.exceptions.ResourceNotFoundException;
 import com.jupitters.jupittersshops.model.Image;
 import com.jupitters.jupittersshops.repository.ImageRepository;
+import com.jupitters.jupittersshops.service.product.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.sql.SQLException;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService implements IImageService{
     private final ImageRepository imageRepository;
+    private final IProductService productService;
 
     @Override
     public Image getImageById(Long id) {
@@ -22,7 +28,8 @@ public class ImageService implements IImageService{
     public void deleteImageById(Long id) {
         imageRepository.findById(id)
                 .ifPresentOrElse(imageRepository::delete,
-                        ()-> {throw new ResourceNotFoundException("Image not found!");});
+                        ()-> {throw new ResourceNotFoundException("Image not found!");
+                });
     }
 
     @Override
@@ -31,7 +38,15 @@ public class ImageService implements IImageService{
     }
 
     @Override
-    public void updateImage(MultipartFile file, Long productId) {
-
+    public void updateImage(MultipartFile file, Long imageId) {
+        Image image = getImageById(imageId);
+        try {
+            image.setFileName(file.getOriginalFilename());
+            image.setFileType(file.getContentType());
+            image.setImage(new SerialBlob(file.getBytes()));
+            imageRepository.save(image);
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
