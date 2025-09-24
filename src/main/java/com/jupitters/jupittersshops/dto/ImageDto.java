f9117d495dd5760810d8fcf99c836e1f98ac5765
jupitters <sampaiobale@gmail.com>
2025-09-24 14:55:08 -0300

    image dto

--- src/main/java/com/jupitters/jupittersshops/dto/ImageDto.java
+++ src/main/java/com/jupitters/jupittersshops/dto/ImageDto.java
@@ -1,4 +1,10 @@
 package com.jupitters.jupittersshops.dto;
 
+import lombok.Data;
+
+@Data
 public class ImageDto {
+    private Long imageId;
+    private String imageName;
+    private String downloadUrl;
 }
