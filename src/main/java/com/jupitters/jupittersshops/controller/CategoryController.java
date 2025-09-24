982d1953b96af818ef8d3d0962ca73ec6f68b058
jupitters <sampaiobale@gmail.com>
2025-09-24 17:12:34 -0300

    category controller

--- src/main/java/com/jupitters/jupittersshops/controller/CategoryController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/CategoryController.java
@@ -1,4 +1,35 @@
 package com.jupitters.jupittersshops.controller;
 
+import com.jupitters.jupittersshops.model.Category;
+import com.jupitters.jupittersshops.response.ApiResponse;
+import com.jupitters.jupittersshops.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories(){
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found!", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Error:", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+
 }
