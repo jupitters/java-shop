f788756fd41768392e7517236b5a8f4d9a4194a4
jupitters <sampaiobale@gmail.com>
2025-09-24 10:58:56 -0300

    category service methods

--- src/main/java/com/jupitters/jupittersshops/service/category/CategoryService.java
+++ src/main/java/com/jupitters/jupittersshops/service/category/CategoryService.java
@@ -1,4 +1,38 @@
 package com.jupitters.jupittersshops.service.category;
 
-public class CategoryService {
+import com.jupitters.jupittersshops.model.Category;
+
+import java.util.List;
+
+public class CategoryService extends ICategoryService {
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return List.of();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+
+    }
 }
--- src/main/java/com/jupitters/jupittersshops/service/category/ICategoryService.java
+++ src/main/java/com/jupitters/jupittersshops/service/category/ICategoryService.java
@@ -1,4 +1,14 @@
 package com.jupitters.jupittersshops.service.category;
 
+import com.jupitters.jupittersshops.model.Category;
+
+import java.util.List;
+
 public interface ICategoryService {
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category);
+    void deleteCategory(Long id);
 }
