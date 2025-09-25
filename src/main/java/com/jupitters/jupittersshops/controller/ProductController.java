bb1648221bf6cf774926ea12ba59ba9299525c88
jupitters <sampaiobale@gmail.com>
2025-09-25 14:56:42 -0300

    product controller: get all products

--- src/main/java/com/jupitters/jupittersshops/controller/ProductController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/ProductController.java
@@ -1,16 +1,33 @@
 package com.jupitters.jupittersshops.controller;
 
+import com.jupitters.jupittersshops.model.Product;
+import com.jupitters.jupittersshops.response.ApiResponse;
 import com.jupitters.jupittersshops.service.product.IProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/products")
 public class ProductController {
-    private IProductService productService;
+    private final IProductService productService;
 
-    public ResponseEntity
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts() {
+        try {
+            List<Product> products = productService.getAllProducts();
+            return ResponseEntity.ok(new ApiResponse("Success", products));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse(e.getMessage(), INTERNAL_SERVER_ERROR));
+        }
+    }
 }
