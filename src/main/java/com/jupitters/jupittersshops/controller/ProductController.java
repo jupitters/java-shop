4d9392a90dedbe8895ab031fe626d007687f3713
jupitters <sampaiobale@gmail.com>
2025-09-27 16:19:01 -0300

    product controller

--- src/main/java/com/jupitters/jupittersshops/controller/ProductController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/ProductController.java
@@ -93,7 +93,20 @@ public class ProductController {
         }
      }
 
-
+     @GetMapping("/brand/{brandName}")
+     public ResponseEntity<ApiResponse> getProductByBrand(@PathVariable String brandName) {
+        try {
+            List<Product> products = productService.getProductByBrand(brandName);
+            if (products.isEmpty()){
+                return ResponseEntity.status(NOT_FOUND)
+                        .body(new ApiResponse("Products not found", null));
+            }
+            return ResponseEntity.ok(new ApiResponse("Found!", products));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+     }
 
      @GetMapping("/c/{categoryName}/b/{brandName}")
      public ResponseEntity<ApiResponse> getProductByCategoryAndBrand(@PathVariable String categoryName, @PathVariable String brandName) {
