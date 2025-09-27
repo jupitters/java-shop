68e45ff6f2c31008d58fa28494d38b5897fbea12
jupitters <sampaiobale@gmail.com>
2025-09-27 16:19:39 -0300

    image controller

--- src/main/java/com/jupitters/jupittersshops/controller/ImageController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/ImageController.java
@@ -49,21 +49,7 @@ public class ImageController {
                 .body(resource);
     }
 
-    @PutMapping("/image/{imageId}/update")
-    public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
-        try {
-            Image image = imageService.getImageById(imageId);
-            if(image != null){
-                imageService.updateImage(file, imageId);
-                return ResponseEntity.ok(new ApiResponse("Update Success!", null));
-            }
-        } catch (ResourceNotFoundException e) {
-            return ResponseEntity.status(NOT_FOUND)
-                    .body(new ApiResponse(e.getMessage(), null));
-        }
-        return ResponseEntity.status(INTERNAL_SERVER_ERROR)
-                .body(new ApiResponse("Update Failed!", INTERNAL_SERVER_ERROR));
-    }
+
 
     @DeleteMapping("/image/{imageId}/delete")
     public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId) {
