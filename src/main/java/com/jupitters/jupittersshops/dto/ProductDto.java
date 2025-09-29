42c0f18309e31bb30e1a09a9b7785e998f1bb5ff
jupitters <sampaiobale@gmail.com>
2025-09-29 15:52:17 -0300

    product dto

--- src/main/java/com/jupitters/jupittersshops/dto/ProductDto.java
+++ src/main/java/com/jupitters/jupittersshops/dto/ProductDto.java
@@ -6,10 +6,12 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Data
 public class ProductDto {
     private Long id;
     private String name;
