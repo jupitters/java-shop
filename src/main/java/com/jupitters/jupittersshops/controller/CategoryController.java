2f73977e3c38e1ca0629c0facd9bbc5cdae04ccb
jupitters <sampaiobale@gmail.com>
2025-09-29 15:45:37 -0300

    category controller

--- src/main/java/com/jupitters/jupittersshops/controller/CategoryController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/CategoryController.java
@@ -22,7 +22,15 @@ public class CategoryController {
     private final ICategoryService categoryService;
 
     @GetMapping("/all")
-
+    public ResponseEntity<ApiResponse> getAllCategories(){
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found!", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Error:", INTERNAL_SERVER_ERROR));
+        }
+    }
 
     @PostMapping("/add")
     public ResponseEntity<ApiResponse> addCategory(@RequestBody Category category) {
