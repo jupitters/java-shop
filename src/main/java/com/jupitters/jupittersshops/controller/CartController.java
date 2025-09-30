17ece1c07fe5bd8bb6d735cf3ad013f45fb9ccbe
jupitters <sampaiobale@gmail.com>
2025-09-30 17:48:32 -0300

    cart controller: get cart

--- src/main/java/com/jupitters/jupittersshops/controller/CartController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/CartController.java
@@ -7,6 +7,7 @@ import com.jupitters.jupittersshops.service.cart.CartService;
 import com.jupitters.jupittersshops.service.cart.ICartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 public class CartController {
     private final ICartService cartService;
 
+    @GetMapping("/id")
     public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId) {
         try {
             Cart cart = cartService.getCart(cartId);
