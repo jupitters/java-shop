8f35bbafee27af5015a25092d92deb828349761d
jupitters <sampaiobale@gmail.com>
2025-09-30 16:05:22 -0300

    cart item service: remove item from cart

--- src/main/java/com/jupitters/jupittersshops/service/cart/CartItemService.java
+++ src/main/java/com/jupitters/jupittersshops/service/cart/CartItemService.java
@@ -1,9 +1,11 @@
 package com.jupitters.jupittersshops.service.cart;
 
+import com.jupitters.jupittersshops.exceptions.ResourceNotFoundException;
 import com.jupitters.jupittersshops.model.Cart;
 import com.jupitters.jupittersshops.model.CartItem;
 import com.jupitters.jupittersshops.model.Product;
 import com.jupitters.jupittersshops.repository.CartItemRepository;
+import com.jupitters.jupittersshops.repository.CartRepository;
 import com.jupitters.jupittersshops.service.product.IProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ public class CartItemService implements ICartItemService{
     private final CartItemRepository cartItemRepository;
     private final IProductService productService;
     private final ICartService cartService;
+    private final CartRepository cartRepository;
 
     @Override
     public void addItemToCart(Long cartId, Long productId, Integer quantity) {
@@ -33,11 +36,25 @@ public class CartItemService implements ICartItemService{
         else {
             cartItem.setQuantity(cartItem.getQuantity() + quantity);
         }
+
+        cartItem.setTotalPrice();
+        cart.addItem(cartItem);
+        cartItemRepository.save(cartItem);
+        cartRepository.save(cart);
     }
 
     @Override
     public void removeItemFromCart(Long cartId, Long productId) {
-
+        Cart cart = cartService.getCart(cartId);
+        CartItem itemToRemove = cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct()
+                        .getId()
+                        .equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found!"));
+        cart.removeItem(itemToRemove);
+        cartRepository.save(cart);
     }
 
     @Override
