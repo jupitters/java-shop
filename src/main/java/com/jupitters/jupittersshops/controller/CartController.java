62ac4796d457fa0f3ffbf5fa4dbee0ffd9486a3f
jupitters <sampaiobale@gmail.com>
2025-09-30 16:28:14 -0300

    cart controller: get cart

---
+++ src/main/java/com/jupitters/jupittersshops/controller/CartController.java
@@ -0,0 +1,32 @@
+package com.jupitters.jupittersshops.controller;
+
+import com.jupitters.jupittersshops.exceptions.ResourceNotFoundException;
+import com.jupitters.jupittersshops.model.Cart;
+import com.jupitters.jupittersshops.response.ApiResponse;
+import com.jupitters.jupittersshops.service.cart.CartService;
+import com.jupitters.jupittersshops.service.cart.ICartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/carts")
+public class CartController {
+    private final ICartService cartService;
+
+    public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId) {
+        try {
+            Cart cart = cartService.getCart(cartId);
+            return ResponseEntity.ok(new ApiResponse("Cart found: ", cart));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+}
