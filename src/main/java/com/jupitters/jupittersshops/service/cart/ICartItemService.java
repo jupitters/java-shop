e8aad75844fa676c621afbd87048cd7589f0f949
jupitters <sampaiobale@gmail.com>
2025-09-30 14:36:24 -0300

    cart item service

--- src/main/java/com/jupitters/jupittersshops/service/cart/ICartItemService.java
+++ src/main/java/com/jupitters/jupittersshops/service/cart/ICartItemService.java
@@ -1,4 +1,10 @@
 package com.jupitters.jupittersshops.service.cart;
 
+import com.jupitters.jupittersshops.model.CartItem;
+
 public interface ICartItemService {
+    void addItemToCart(Long cartId, Long productId, Integer quantity);
+    void removeItemFromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, Integer quantity);
+
 }
