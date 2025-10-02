184953c2035a6e1b38e09a924201cd2c6ddded70
jupitters <sampaiobale@gmail.com>
2025-10-02 17:21:04 -0300

    order item model

--- src/main/java/com/jupitters/jupittersshops/model/OrderItem.java
+++ src/main/java/com/jupitters/jupittersshops/model/OrderItem.java
@@ -1,4 +1,29 @@
 package com.jupitters.jupittersshops.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
 }
