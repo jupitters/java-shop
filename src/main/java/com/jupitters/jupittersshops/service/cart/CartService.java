3afe501b59e9161f151608a78e8ddbdad3cbb158
jupitters <sampaiobale@gmail.com>
2025-10-02 15:04:31 -0300

    cart service: cart id generator

--- src/main/java/com/jupitters/jupittersshops/service/cart/CartService.java
+++ src/main/java/com/jupitters/jupittersshops/service/cart/CartService.java
@@ -9,12 +9,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 @RequiredArgsConstructor
 public class CartService implements ICartService{
     private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
+    private final AtomicLong cartIdgenerator = new AtomicLong(0);
 
     @Override
     public Cart getCart(Long id) {
@@ -37,5 +39,12 @@ public class CartService implements ICartService{
     public BigDecimal getTotalPrice(Long id) {
         Cart cart = getCart(id);
         return cart.getTotalAmount();
+    }
+
+    public Long initializeNewCart() {
+        Cart newCart = new Cart();
+        Long newCartId = cartIdgenerator.getAndIncrement();
+        newCart.setId(newCartId);
+        return cartRepository.save(newCart).getId();
     }
 }
