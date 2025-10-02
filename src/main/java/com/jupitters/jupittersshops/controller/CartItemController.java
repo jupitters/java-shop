9b51a32c1c2b57b7f99b0fb5ddeb8c7ef155b63e
jupitters <sampaiobale@gmail.com>
2025-10-02 14:39:48 -0300

    cart item controller: add item to cart

--- src/main/java/com/jupitters/jupittersshops/controller/CartItemController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/CartItemController.java
@@ -1,5 +1,6 @@
 package com.jupitters.jupittersshops.controller;
 
+import com.jupitters.jupittersshops.exceptions.ResourceNotFoundException;
 import com.jupitters.jupittersshops.model.Cart;
 import com.jupitters.jupittersshops.model.CartItem;
 import com.jupitters.jupittersshops.response.ApiResponse;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -17,10 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CartItemController {
     private ICartItemService cartItemService;
 
-    public ResponseEntity<ApiResponse> addItemToCart(@RequestParam Cart cartId,
+    public ResponseEntity<ApiResponse> addItemToCart(@RequestParam Long cartId,
                                                      @RequestParam Long productId,
                                                      @RequestParam Integer quantity){
-        cartItemService.addItemToCart(cartId, productId, quantity);
-        return ResponseEntity.ok(new ApiResponse("Added successfull!", null));
+        try {
+            cartItemService.addItemToCart(cartId, productId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Added successfull!", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
     }
 }
