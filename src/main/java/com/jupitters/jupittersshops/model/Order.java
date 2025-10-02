416cd14e7564898212fee6eb43e0c2bfffc1c88e
jupitters <sampaiobale@gmail.com>
2025-10-02 17:15:53 -0300

    order model

--- src/main/java/com/jupitters/jupittersshops/model/Order.java
+++ src/main/java/com/jupitters/jupittersshops/model/Order.java
@@ -1,13 +1,28 @@
 package com.jupitters.jupittersshops.model;
 
 import com.jupitters.jupittersshops.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
     private LocalDate orderDate;
     private BigDecimal totalAmount;
+    @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems;
 }
