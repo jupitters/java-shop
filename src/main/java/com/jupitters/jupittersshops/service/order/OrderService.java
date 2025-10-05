68f26221cbf7b365a4c8c17200e10478ec89ecf6
jupitters <sampaiobale@gmail.com>
2025-10-04 23:18:47 -0300

    order service

--- src/main/java/com/jupitters/jupittersshops/service/order/OrderService.java
+++ src/main/java/com/jupitters/jupittersshops/service/order/OrderService.java
@@ -25,7 +25,21 @@ public class OrderService implements IOrderService{
     private final ProductRepository productRepository;
     private final CartService cartService;
 
+    @Transactional
+    @Override
+    public Order placeOrder(Long userId) {
+        Cart cart = cartService.getCart(userId);
+        Order order = createOrder(cart);
+        List<OrderItem> orderItemList = createOrderItems(order,  cart);
 
+        order.setOrderItems(new HashSet<>(orderItemList));
+        order.setTotalAmount(calculateTotalAmount(orderItemList));
+        Order savedOrder = orderRepository.save(order);
+
+        cartService.clearCart(cart.getId());
+
+        return savedOrder;
+    }
 
     private Order createOrder(Cart cart) {
         Order order = new Order();
