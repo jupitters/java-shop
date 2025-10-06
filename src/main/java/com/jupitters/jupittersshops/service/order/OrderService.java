3406cad6cc5a7bbfba10198d163822249c6381fd
jupitters <sampaiobale@gmail.com>
2025-10-06 14:31:43 -0300

    order service

--- src/main/java/com/jupitters/jupittersshops/service/order/OrderService.java
+++ src/main/java/com/jupitters/jupittersshops/service/order/OrderService.java
@@ -77,9 +77,6 @@ public class OrderService implements IOrderService{
                 .orElseThrow(() -> new ResourceNotFoundException("Order not found"));
     }
 
-    @Override
-    public List<Order> getUserOrders(Long userId) {
-        return orderRepository.findByUserId(userId);
-    }
+
 
 }
