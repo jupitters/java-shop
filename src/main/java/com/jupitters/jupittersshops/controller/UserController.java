52b83bd708154cd149aad0677790f738c07e01ca
jupitters <sampaiobale@gmail.com>
2025-10-06 16:05:30 -0300

    user controller

--- src/main/java/com/jupitters/jupittersshops/controller/UserController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.jupitters.jupittersshops.controller;
 
+import com.jupitters.jupittersshops.model.User;
+import com.jupitters.jupittersshops.response.ApiResponse;
+import com.jupitters.jupittersshops.service.user.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("${api.prefix}/users")
 public class UserController {
+    private final IUserService userService;
 
+    public ResponseEntity<ApiResponse> getUserById(Long userId){
+        User user = userService.getUserById(userId);
+        return ResponseEntity.ok(new ApiResponse("Success!", user));
+    }
 }
