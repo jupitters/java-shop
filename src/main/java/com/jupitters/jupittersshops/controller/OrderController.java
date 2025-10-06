c005db06a4b5d0732cf2bb270e807799146d4dbf
jupitters <sampaiobale@gmail.com>
2025-10-06 15:14:39 -0300

    order controller: create

--- src/main/java/com/jupitters/jupittersshops/controller/OrderController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/OrderController.java
@@ -1,6 +1,10 @@
 package com.jupitters.jupittersshops.controller;
 
+import com.jupitters.jupittersshops.model.Order;
+import com.jupitters.jupittersshops.response.ApiResponse;
+import com.jupitters.jupittersshops.service.order.IOrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("${api.prefix}/orders")
 public class OrderController {
+    private final IOrderService orderService;
 
+    public ResponseEntity<ApiResponse> createOrder(Long userId) {
+        Order order = orderService.placeOrder(userId);
+        return ResponseEntity.ok(new ApiResponse("Item Order success!", order));
+    }
 }
