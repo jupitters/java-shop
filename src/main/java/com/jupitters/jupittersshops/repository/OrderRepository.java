2c6734748b5dbf3c1e3d4dea1273697dc2a4eee9
jupitters <sampaiobale@gmail.com>
2025-10-06 14:31:35 -0300

    order service

--- src/main/java/com/jupitters/jupittersshops/repository/OrderRepository.java
+++ src/main/java/com/jupitters/jupittersshops/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.jupitters.jupittersshops.repository;
 
 import com.jupitters.jupittersshops.model.Order;
+import com.jupitters.jupittersshops.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> user(User user);
+
+    List<Order> findByUserId(Long userId);
 }
--- src/main/java/com/jupitters/jupittersshops/service/order/IOrderService.java
+++ src/main/java/com/jupitters/jupittersshops/service/order/IOrderService.java
@@ -2,7 +2,11 @@ package com.jupitters.jupittersshops.service.order;
 
 import com.jupitters.jupittersshops.model.Order;
 
+import java.util.List;
+
 public interface IOrderService {
     Order placeOrder(Long userId);
     Order getOrder(Long orderId);
+
+    List<Order> getUserOrders(Long userId);
 }
