f966c38f67c606311d0b7fded7f5b4c5ce2ba1db
jupitters <sampaiobale@gmail.com>
2025-10-06 14:55:13 -0300

    user service

---
+++ src/main/java/com/jupitters/jupittersshops/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.jupitters.jupittersshops.repository;
+
+import com.jupitters.jupittersshops.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
--- src/main/java/com/jupitters/jupittersshops/service/user/UserService.java
+++ src/main/java/com/jupitters/jupittersshops/service/user/UserService.java
@@ -1,4 +1,36 @@
 package com.jupitters.jupittersshops.service.user;
 
-public class UserService {
+import com.jupitters.jupittersshops.exceptions.ResourceNotFoundException;
+import com.jupitters.jupittersshops.model.User;
+import com.jupitters.jupittersshops.repository.UserRepository;
+import com.jupitters.jupittersshops.request.CreateUserRequest;
+import com.jupitters.jupittersshops.request.UserUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService{
+    private final UserRepository userRepository;
+
+    @Override
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    @Override
+    public User createUser(CreateUserRequest request) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(UserUpdateRequest request, Long userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+
+    }
 }
