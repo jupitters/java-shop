bac4b1e23b378523cd297090daf49d6c5c89ebe1
jupitters <sampaiobale@gmail.com>
2025-10-06 16:34:12 -0300

    user dto

---
+++ src/main/java/com/jupitters/jupittersshops/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.jupitters.jupittersshops.dto;
+
+import com.jupitters.jupittersshops.model.Cart;
+import com.jupitters.jupittersshops.model.Order;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<OrderDto> orders;
+    private CartDto cart;
+}
