ade9db712cbc39287a91c0f7776f656331442bf8
jupitters <sampaiobale@gmail.com>
2025-10-07 17:31:09 -0300

    role model

--- src/main/java/com/jupitters/jupittersshops/security/user/ShopUserDetailsService.java
+++ src/main/java/com/jupitters/jupittersshops/security/user/ShopUserDetailsService.java
@@ -1,4 +1,13 @@
 package com.jupitters.jupittersshops.security.user;
 
-public class ShopUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class ShopUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
