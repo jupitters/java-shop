945371d745585540137a8f45787f19158c458d25
jupitters <sampaiobale@gmail.com>
2025-10-07 17:42:14 -0300

    shop user details

--- src/main/java/com/jupitters/jupittersshops/security/user/ShopUserDetails.java
+++ src/main/java/com/jupitters/jupittersshops/security/user/ShopUserDetails.java
@@ -1,5 +1,6 @@
 package com.jupitters.jupittersshops.security.user;
 
+import com.jupitters.jupittersshops.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -7,6 +8,15 @@ import java.util.Collection;
 import java.util.List;
 
 public class ShopUserDetails implements UserDetails {
+    private Long id;
+    private String email;
+    private String password;
+
+    private Collection<GrantedAuthority> authorities;
+
+    public static ShopUserDetails buildUserDetails(User user){
+
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
