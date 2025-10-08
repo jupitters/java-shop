baddf399becbf5d71a3609b3c5f2bc0e4c0de709
jupitters <sampaiobale@gmail.com>
2025-10-08 15:16:40 -0300

    shop user details: build user details

--- src/main/java/com/jupitters/jupittersshops/security/user/ShopUserDetails.java
+++ src/main/java/com/jupitters/jupittersshops/security/user/ShopUserDetails.java
@@ -40,7 +40,7 @@ public class ShopUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return authorities;
     }
 
     @Override
