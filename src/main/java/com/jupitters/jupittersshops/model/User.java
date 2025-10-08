6a02789463ab479b4b083544b6dbeedf1aeee9b3
jupitters <sampaiobale@gmail.com>
2025-10-08 15:23:11 -0300

    user model

--- src/main/java/com/jupitters/jupittersshops/model/User.java
+++ src/main/java/com/jupitters/jupittersshops/model/User.java
@@ -32,6 +32,10 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Order> orders;
 
-
+    @ManyToMany(fetch = FetchType.EAGER, cascade =
+            {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
     private Collection<Role> roles = new HashSet<>();
 }
