9d7916aaa81753a0535c0c30fce1d507e587d77e
jupitters <sampaiobale@gmail.com>
2025-10-08 17:45:22 -0300

    auth token filter

---
+++ src/main/java/com/jupitters/jupittersshops/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,32 @@
+package com.jupitters.jupittersshops.security.jwt;
+
+import com.jupitters.jupittersshops.security.user.ShopUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private JwtUtils jwtUtils;
+    private ShopUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+        return null;
+    }
+}
