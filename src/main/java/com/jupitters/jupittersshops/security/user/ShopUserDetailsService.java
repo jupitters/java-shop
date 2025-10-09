3e3a4512c1e6b4228c821920eccb42a01aa231f7
jupitters <sampaiobale@gmail.com>
2025-10-09 11:12:34 -0300

    user details

--- src/main/java/com/jupitters/jupittersshops/security/user/ShopUserDetailsService.java
+++ src/main/java/com/jupitters/jupittersshops/security/user/ShopUserDetailsService.java
@@ -15,5 +15,10 @@ import java.util.Optional;
 public class ShopUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
-
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
 }
