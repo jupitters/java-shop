4d5999bfff59acb2975d8cf0097927a1c0fff2b4
jupitters <sampaiobale@gmail.com>
2025-10-10 15:33:58 -0300

    shop config

--- src/main/java/com/jupitters/jupittersshops/security/config/ShopConfig.java
+++ src/main/java/com/jupitters/jupittersshops/security/config/ShopConfig.java
@@ -1,11 +1,15 @@
 package com.jupitters.jupittersshops.security.config;
 
+import com.jupitters.jupittersshops.security.user.ShopUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ShopConfig {
+    private final ShopUserDetailsService userDetailsService;
 
     @Bean
     public ModelMapper modelMapper() {
