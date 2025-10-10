7568fa37238a1a128e7b21008994efb779d82781
jupitters <sampaiobale@gmail.com>
2025-10-10 15:28:30 -0300

    jwt utils

--- src/main/java/com/jupitters/jupittersshops/security/jwt/JwtUtils.java
+++ src/main/java/com/jupitters/jupittersshops/security/jwt/JwtUtils.java
@@ -36,9 +36,7 @@ public class JwtUtils {
                 .signWith(key(), SignatureAlgorithm.HS256).compact();
     }
 
-    private Key key() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
-    }
+
 
     public String getUsernameFromToken(String token) {
         return Jwts.parser()
