9d7beda89186bbc5d49fd102b165955b7e15e67c
jupitters <sampaiobale@gmail.com>
2025-10-12 00:47:33 -0300

    category service

--- src/main/java/com/jupitters/jupittersshops/service/category/CategoryService.java
+++ src/main/java/com/jupitters/jupittersshops/service/category/CategoryService.java
@@ -50,5 +50,10 @@ public class CategoryService implements ICategoryService {
                 .orElseThrow(()-> new ResourceNotFoundException("Category not found!"));
     }
 
-
+    @Override
+    public void deleteCategory(Long id) {
+        categoryRepository.findById(id)
+                .ifPresentOrElse(categoryRepository::delete,
+                        ()-> {throw new ResourceNotFoundException("Category not found!");});
+    }
 }
