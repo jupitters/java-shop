ef532fb1680f6fa4e5197f6d650929bfdfe64376
jupitters <sampaiobale@gmail.com>
2025-10-13 16:21:48 -0300

    login

--- src/main/java/com/jupitters/jupittersshops/controller/AuthController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/AuthController.java
@@ -1,4 +1,36 @@
 package com.jupitters.jupittersshops.controller;
 
+import com.jupitters.jupittersshops.request.LoginRequest;
+import com.jupitters.jupittersshops.response.ApiResponse;
+import com.jupitters.jupittersshops.response.JwtResponse;
+import com.jupitters.jupittersshops.security.jwt.JwtUtils;
+import com.jupitters.jupittersshops.security.user.ShopUserDetails;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/auth")
 public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+
+    public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginRequest request) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateTokenForUser(authentication);
+        ShopUserDetails userDetails = (ShopUserDetails) authentication.getPrincipal();
+        JwtResponse jwtResponse = new JwtResponse(userDetails.getId(), jwt);
+        return ResponseEntity.ok(new ApiResponse("Login Successful!", jwtResponse));
+
+    }
 }
