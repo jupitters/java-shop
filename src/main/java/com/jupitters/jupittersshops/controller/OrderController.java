10786c2c6a64d4b3858f246cd87dc8406203f906
jupitters <sampaiobale@gmail.com>
2025-10-13 15:04:00 -0300

    order controller

--- src/main/java/com/jupitters/jupittersshops/controller/OrderController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/OrderController.java
@@ -20,7 +20,17 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 public class OrderController {
     private final IOrderService orderService;
 
-
+    @PostMapping("/order")
+    public ResponseEntity<ApiResponse> createOrder(@RequestParam Long userId) {
+        try {
+            Order order = orderService.placeOrder(userId);
+            OrderDto orderDto = orderService.convertToDto(order);
+            return ResponseEntity.ok(new ApiResponse("Item Order success!", orderDto));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Error occurred: ", e.getMessage()));
+        }
+    }
 
     @GetMapping("/id/{orderId}")
     public ResponseEntity<ApiResponse> getOrderById(@PathVariable Long orderId) {
