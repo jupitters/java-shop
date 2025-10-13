2d08f04cb1bfce3ec95fdf3a2fa4737c24217409
jupitters <sampaiobale@gmail.com>
2025-10-13 15:28:04 -0300

    user service: password encoder

--- src/main/java/com/jupitters/jupittersshops/service/user/UserService.java
+++ src/main/java/com/jupitters/jupittersshops/service/user/UserService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -20,6 +21,7 @@ import java.util.Optional;
 public class UserService implements IUserService{
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User getUserById(Long userId) {
@@ -36,7 +38,7 @@ public class UserService implements IUserService{
                     user.setFirstName(request.getFirstName());
                     user.setLastName(request.getLastName());
                     user.setEmail(request.getEmail());
-                    user.setPassword(request.getPassword());
+                    user.setPassword(passwordEncoder.encode(request.getPassword()));
                     return userRepository.save(user);
                 }).orElseThrow(() -> new AlreadyExistsException(request.getEmail()+ " already exists."));
     }
