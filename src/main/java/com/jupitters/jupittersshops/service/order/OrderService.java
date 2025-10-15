0b6f7565c904b4cbd8bc82456c19d86687087f2e
jupitters <sampaiobale@gmail.com>
2025-10-15 10:48:17 -0300

    order service

--- src/main/java/com/jupitters/jupittersshops/service/order/OrderService.java
+++ src/main/java/com/jupitters/jupittersshops/service/order/OrderService.java
@@ -74,12 +74,7 @@ public class OrderService implements IOrderService{
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
-    @Override
-    public OrderDto getOrder(Long orderId) {
-        return orderRepository.findById(orderId)
-                .map(this :: convertToDto)
-                .orElseThrow(() -> new ResourceNotFoundException("Order not found"));
-    }
+
 
     @Override
     public List<OrderDto> getUserOrders(Long userId) {
