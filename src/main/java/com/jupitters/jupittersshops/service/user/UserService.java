0b35feeca053d31352088497ca729ec51d474987
jupitters <sampaiobale@gmail.com>
2025-10-16 23:50:42 -0300

    user service

--- src/main/java/com/jupitters/jupittersshops/service/user/UserService.java
+++ src/main/java/com/jupitters/jupittersshops/service/user/UserService.java
@@ -63,10 +63,7 @@ public class UserService implements IUserService{
                 });
     }
 
-    @Override
-    public UserDto convertUserToDto(User user){
-        return modelMapper.map(user, UserDto.class);
-    }
+
 
     @Override
     public User getAuthenticatedUser() {
