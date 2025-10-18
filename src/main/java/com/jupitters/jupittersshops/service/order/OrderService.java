4c2fcb007cad4798c7d141aac793be212fdd525d
jupitters <sampaiobale@gmail.com>
2025-10-18 13:59:53 -0300

    product service

--- src/main/java/com/jupitters/jupittersshops/service/order/OrderService.java
+++ src/main/java/com/jupitters/jupittersshops/service/order/OrderService.java
@@ -88,9 +88,6 @@ public class OrderService implements IOrderService{
                 .map(this :: convertToDto).toList();
     }
 
-    @Override
-    public OrderDto convertToDto(Order order) {
-        return modelMapper.map(order, OrderDto.class);
-    }
+
 
 }
