8a9256b497c619e1929177c362e51ebbd1b1122b
jupitters <sampaiobale@gmail.com>
2025-11-02 17:51:31 -0300

    cart controller

--- src/main/java/com/jupitters/jupittersshops/controller/CartController.java
+++ src/main/java/com/jupitters/jupittersshops/controller/CartController.java
@@ -19,7 +19,16 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 public class CartController {
     private final ICartService cartService;
 
-    
+    @GetMapping("/id/{cartId}")
+    public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId) {
+        try {
+            Cart cart = cartService.getCart(cartId);
+            return ResponseEntity.ok(new ApiResponse("Cart found: ", cart));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 
     @DeleteMapping("/id/clear/{cartId}")
     public ResponseEntity<ApiResponse> cleanCart(@PathVariable Long cartId){
