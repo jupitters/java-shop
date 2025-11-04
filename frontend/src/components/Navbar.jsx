3a52d84d93b7b1a9a2b5ae8051fb844e7f7ad4f9
jupitters <sampaiobale@gmail.com>
2025-11-04 14:57:11 -0300

    testing bootstrap

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1 className='mt-2 d-flex justify-content-center'>oi</h1>
+        <Button>oi</Button>
+    </div>
+  )
+}
+
+export default Navbar
