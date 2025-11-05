a0e1b092dbf6e9cbd5238210869e1414f615fa6c
jupitters <sampaiobale@gmail.com>
2025-11-05 15:20:02 -0300

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,26 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const Navbar = () => {
+  const token = localStorage.getItem("token");
+  let isAdmin = false;
+
+  if (token) {
+    try{
+      const decoded = jwtDecode(token);
+      isAdmin = decoded.roles && decoded.roles.includes("ROLE_ADMIN");
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <nav style={{display: "flex", gap: "1rem", padding: "10px"}}>
-        <Link to="/">Home</Link>
-        <Link to="/cart">Cart</Link>
-        <Link to="/profile">Profile</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/cart">Cart</NavLink>
+        <NavLink to="/profile">Profile</NavLink>
+
+        {isAdmin && <NavLink to="/add-product">Add Product</NavLink>}
     </nav>
   )
 }
