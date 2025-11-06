21e96ccf93c8b825ae7ec48996234db7141ea4d9
jupitters <sampaiobale@gmail.com>
2025-11-05 22:44:00 -0300

    trying to get cart

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,11 +8,13 @@ export const AppContext = createContext();
 export const AppProvider = ({ children }) => {
   const apiUrl = "http://localhost:9191/api/v1"
   const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem("token"));
+  const token = localStorage.getItem("token")
   const [loading, setLoading] = useState(false)
   const [user, setUser] = useState("")
   const [products, setProducts] = useState([])
   const [selectedProduct, setSelectedProduct] = useState(null)
   const [showModal, setShowModal] = useState(false)
+  const [cart, setCart] = useState([])
 
   const login = (token, userId) => {
     localStorage.setItem("token", token);
@@ -54,6 +56,21 @@ export const AppProvider = ({ children }) => {
     setLoading(false)
   }
 
+  const fetchCart = async (cartId, token) => {
+    setLoading(true)
+      try{
+        const { data } = await axios.get(`${apiUrl}/carts/id/${cartId}`, null, {
+          headers: {
+            Authorization: `Bearer ${token}` 
+          }
+        })
+        setCart(data.data)
+      } catch (error) {
+        console.log(error)
+      }
+    setLoading(false)
+  }
+
   const addToCart = async (id, token) => {
     try{
       await axios.post(`${apiUrl}/cartItems/item/add?itemId=${id}&quantity=1`, null, {
@@ -78,10 +95,17 @@ export const AppProvider = ({ children }) => {
 
   useEffect(() => {
     fetchProducts()
-  }, [])
+    fetchUser(token)
+  }, [token])
+
+  useEffect(() => {
+    if(user && user.cart){
+      fetchCart(user.cart.cartId, token)
+    }
+  }, [user])
 
   return (
-    <AppContext.Provider value={{ isAuthenticated, login, logout,fetchUser, user, products, loading, addToCart, showModal, selectProduct, selectedProduct, closeModal }}>
+    <AppContext.Provider value={{ isAuthenticated, login, logout,fetchUser, user, products, loading, addToCart, showModal, selectProduct, selectedProduct, closeModal, fetchCart, cart }}>
       {children}
     </AppContext.Provider>
   );
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import Navbar from '../components/Navbar'
+import { AppContext } from '../context/AppContext'
 
 const Cart = () => {
+  const { cart} = useContext(AppContext)
+
+  // console.log(cart)
   return (
     <div>
-        Cart
+      oi
     </div>
   )
 }
